5c95bfefef63f79a6629a467737cbf795f87e4dc
defifofum <defifofum0@gmail.com>
2021-10-18 15:27:10 -0400

    chore: Clean exmaple

--- examples/contractAbi.ts
+++ examples/contractAbi.ts
@@ -6,11 +6,12 @@ const { baseUrl: BASE_URL, apiKey: API_KEY } = getConfig('bsc');
 
 (async function () {
   const etherscanService = new EtherscanService(BASE_URL, API_KEY);
-  const contractSourceCode = await etherscanService.getContractSourceCode('0x5c8D727b265DBAfaba67E050f2f739cAeEB4A6F9'); // MasterApe
+  const fullContractDetails = await etherscanService.getFullContractDetails('0x5c8D727b265DBAfaba67E050f2f739cAeEB4A6F9'); // MasterApe
 
-  const parsedAbi = parseABI(contractSourceCode.ABI);
-  console.dir({functions: parsedAbi.functions}, {depth:5});
-
+  for (const eventName of fullContractDetails.parsedAbi.eventList) {
+      console.dir(fullContractDetails.parsedAbi.getEventByName(eventName))
+  }
+  
   await Promise.resolve(console.log('🎉'));
   process.exit(0);
 })();
